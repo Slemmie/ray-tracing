5ee4c7168e05a006a7af8fb5202f723a8b0df835
Slemmie <thorvejeneriksen@gmail.com>
2022-03-02 00:20:58 +0100

    implement gp::stst::update_texture()

--- v1.0/src/graphics/render_image.cpp
+++ v1.0/src/graphics/render_image.cpp
@@ -4,6 +4,23 @@
 
 namespace gp {
 	
-	
+	void update_texture(unsigned char* texture_buffer, int texture_width, int texture_height) {
+		m_texture_width = texture_width;
+		m_texture_height = texture_height;
+		m_texture_buffer = texture_buffer;
+		
+		glGenTextures(1, &m_texture_id);
+		glBindTexture(GL_TEXTURE_2D, m_texture_id);
+		
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		
+		glTexImage2D(GL_TEXTURE_2D, GL_RGBA8, texture_width, texture_height,
+		0, GL_RGBA, GL_UNSIGNED_BYTE, m_texture_buffer);
+		
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
 	
 } /// namespace gp
