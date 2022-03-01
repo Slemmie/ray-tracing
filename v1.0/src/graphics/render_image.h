3ac9dd8edfa624706cd4cd1490c6fb9995559553
Slemmie <thorvejeneriksen@gmail.com>
2022-03-02 00:14:01 +0100

    set up render_image.h and render_image.cpp files

---
+++ v1.0/src/graphics/render_image.h
@@ -0,0 +1,20 @@
+// dense code to display a single 2d texture on the entire screen
+// will be abstracted later
+
+#pragma once
+
+namespace gp {
+	
+	class Single_texture_static_renderer {
+		
+	public:
+		
+		
+		
+	private:
+		
+		
+		
+	};
+	
+} /// namespace gp
