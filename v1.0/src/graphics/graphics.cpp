fca9370cc34b29c117f23f06b10ceecc8e9db53b
Slemmie <thorvejeneriksen@gmail.com>
2022-03-01 09:20:22 +0100

    add glfw init call in gp::init()

--- v1.0/src/graphics/graphics.cpp
+++ v1.0/src/graphics/graphics.cpp
@@ -15,6 +15,11 @@ namespace gp {
 		}
 		is_initialized = true;
 		
+		if (!glfwInit()) {
+			std::cerr << "[fatal]: failed to initialize glfw" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		
 		glfwSetErrorCallback([] (int id, const char* message) -> void {
 			std::cerr << "[glfw error]: " << message << " (error ID: " << id << ")" << std::endl;
 		});
