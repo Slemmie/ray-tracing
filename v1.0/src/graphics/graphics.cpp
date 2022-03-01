0054f46f35600e51e8fddaa62986465a474bd639
Slemmie <thorvejeneriksen@gmail.com>
2022-03-01 09:19:13 +0100

    add already initialized check in gp::init()

--- v1.0/src/graphics/graphics.cpp
+++ v1.0/src/graphics/graphics.cpp
@@ -2,10 +2,18 @@
 
 #include "graphics.h"
 
+#include <iostream>
+
 namespace gp {
 	
 	void init() {
-		
+		// make sure we do not re-init
+		static bool is_initialized = false;
+		if (is_initialized) {
+			std::cerr << "[warning]: attempted to re-init graphics" << std::endl;
+			return;
+		}
+		is_initialized = true;
 	}
 	
 } /// namespace gp
