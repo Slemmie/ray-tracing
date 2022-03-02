d06f62201c644794ccf6a20eff0723dc5922bf84
Slemmie <thorvejeneriksen@gmail.com>
2022-03-02 12:52:52 +0100

    add clamp utility function

--- v1.0/src/util/util.h
+++ v1.0/src/util/util.h
@@ -12,6 +12,10 @@ constexpr double deg_to_rad(double degrees) {
 	return degrees * PI / 180.0;
 }
 
+template <typename A> constexpr A clamp(const A& value, const A& min, const A& max) {
+	return value < min ? min : value > max ? max : value;
+}
+
 // as well as the rest of util/...
 
 #include "vec3.h"
