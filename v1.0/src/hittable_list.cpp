b712445d4a86e7e12927cc450b4541f5089d8d3c
Slemmie <thorvejeneriksen@gmail.com>
2022-03-02 12:08:47 +0100

    create hittable list object

---
+++ v1.0/src/hittable_list.cpp
@@ -0,0 +1,19 @@
+// implements hittable_list.h
+
+#include "hittable_list.h"
+
+bool hit(const Rayd& ray, double t_min, double t_max, Hit_record& hit_record) const {
+	Hit_record record;
+	bool any_hits = false;
+	auto closest = t_max;
+	
+	for (const auto& surface : m_surfaces) {
+		if (surface->hit(ray, t_min, closest_so_far, record)) {
+			any_hits = true;
+			closest = record.t;
+			hit_record = record;
+		}
+	}
+	
+	return any_hits;
+}
