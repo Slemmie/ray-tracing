bb84c7f6978e2b13c6e314dc44298ffb5b57c217
Slemmie <thorvejeneriksen@gmail.com>
2022-03-02 11:54:53 +0100

    create sphere surface class

---
+++ v1.0/src/sphere.h
@@ -0,0 +1,32 @@
+// sphere surface
+
+#pragma once
+
+#include "hittable.h"
+#include "ray.h"
+#include "util/vec3.h"
+
+class Sphere : public Hittable {
+	
+public:
+	
+	Sphere() { }
+	
+	Sphere(const vec3d _center, double _radius) :
+	m_center(_center),
+	m_radius(_radius)
+	{ }
+	
+	virtual bool hit(const Rayd& ray, double t_min, double t_max, Hit_record& hit_record) const override;
+	
+	inline vec3d& center() { return m_center; }
+	
+	inline double& radius() { return m_radius; }
+	
+private:
+	
+	vec3d m_center;
+	
+	double m_radius;
+	
+};
