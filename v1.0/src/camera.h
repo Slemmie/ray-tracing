5a982777ebfee358462a1439a446e93a61c97b70
Slemmie <thorvejeneriksen@gmail.com>
2022-03-02 16:47:31 +0100

    add camera position

--- v1.0/src/camera.h
+++ v1.0/src/camera.h
@@ -15,22 +15,24 @@ class Camera {
 	
 public:
 	
-	Camera(double _vertical_fov, double _aspect_ratio) {
+	Camera(vec3d look_from, vec3d look_at, vec3d vup, double _vertical_fov, double _aspect_ratio) {
 		auto theta = deg_to_rad(_vertical_fov);
 		double h = tan(theta / 2.0);
 		double vp_h = 2.0 * h;
 		double vp_w = _aspect_ratio * vp_h;
 		
-		double focal_length = 1.0;
+		auto w = (look_from - look_at).unit_vector();
+		auto u = cross(vup, w).unit_vector();
+		auto v = cross(w, u);
 		
-		m_origin = vec3(0.0, 0.0, 0.0);
-		m_horizontal = vec3(vp_w, 0.0, 0.0);
-		m_vertical = vec3(0.0, vp_h, 0.0);
-		m_low_lef_corner = m_origin - m_horizontal / 2.0 - m_vertical / 2.0 - vec3(0.0, 0.0, focal_length);
+		m_origin = look_from;
+		m_horizontal = vp_w * u;
+		m_vertical = vp_h * v;
+		m_low_lef_corner = m_origin - m_horizontal / 2.0 - m_vertical / 2.0 - w;
 	}
 	
-	constexpr Rayd get_ray(double u, double v) const {
-		return Ray(m_origin, m_low_lef_corner + u * m_horizontal + v * m_vertical - m_origin);
+	constexpr Rayd get_ray(double s, double t) const {
+		return Ray(m_origin, m_low_lef_corner + s * m_horizontal + t * m_vertical - m_origin);
 	}
 	
 private:
