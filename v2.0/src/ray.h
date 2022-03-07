fc57b72dc5abdff5532948cb16f422e6acca715c
Slemmie <thorvejeneriksen@gmail.com>
2022-03-07 11:01:24 +0100

    add time to ray object

--- v2.0/src/ray.h
+++ v2.0/src/ray.h
@@ -16,16 +16,20 @@ public:
 	
 	constexpr Ray() { }
 	
-	constexpr Ray(const vec3 <A>& _origin, const vec3 <A>& _direction) :
+	constexpr Ray(const vec3 <A>& _origin, const vec3 <A>& _direction, double _time = 0.0) :
 	m_origin(_origin),
-	m_direction(_direction)
+	m_direction(_direction),
+	m_time(_time)
 	{ }
 	
-	constexpr const vec3 <A> & origin() const { return m_origin; }
-	inline vec3 <A> & origin() { return m_origin; }
+	constexpr const vec3 <A>& origin() const { return m_origin; }
+	inline vec3 <A>& origin() { return m_origin; }
 	
-	constexpr const vec3 <A> & direction() const { return m_direction; }
-	inline vec3 <A> & direction() { return m_direction; }
+	constexpr const vec3 <A>& direction() const { return m_direction; }
+	inline vec3 <A>& direction() { return m_direction; }
+	
+	constexpr const double& time() const { return m_time; }
+	inline double& time() { return m_time; }
 	
 	template <typename B> constexpr vec3 <A> at(const B& t) const { return origin() + t * direction(); }
 	
@@ -34,6 +38,8 @@ private:
 	vec3 <A> m_origin;
 	
 	vec3 <A> m_direction;
+	
+	double m_time;
 	
 };
 
