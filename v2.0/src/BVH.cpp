d4735e795d2740f55b2f6760321256862bc88783
Slemmie <thorvejeneriksen@gmail.com>
2022-03-07 17:05:19 +0100

    bvh node class hit function implementation

--- v2.0/src/BVH.cpp
+++ v2.0/src/BVH.cpp
@@ -1,3 +1,14 @@
 // implements BVH.h
 
 #include "BVH.h"
+
+bool BVH_node hit(const Rayd& ray, double t_min, double t_max, Hit_record& hit_record) const {
+	if (!m_box.hit(ray, t_min, t_max)) {
+		return false;
+	}
+	
+	bool hit_left = left->hit(ray, t_min, t_max, hit_record);
+	bool hit_right = right->hit(ray, t_min, hit_left ? hit_record.t : t_max, hit_record);
+	
+	return hit_left || hit_right;
+}
