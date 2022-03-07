f7bf70fb467a6bae74ca260b643bc6e56a7f6724
Slemmie <thorvejeneriksen@gmail.com>
2022-03-07 11:11:42 +0100

    add time interval to camera class

--- v2.0/src/camera.h
+++ v2.0/src/camera.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "util/util.h"
+#include "util/random.h"
 
 namespace gp {
 	
@@ -16,7 +17,7 @@ class Camera {
 public:
 	
 	Camera(vec3d look_from, vec3d look_at, vec3d vup, double _vertical_fov, double _aspect_ratio,
-	double _aperture, double _focus_dist) {
+	double _aperture, double _focus_dist, double _time_begin = 0.0, double _time_end = 0.0) {
 		auto theta = deg_to_rad(_vertical_fov);
 		double h = tan(theta / 2.0);
 		double vp_h = 2.0 * h;
@@ -32,12 +33,18 @@ public:
 		m_low_lef_corner = m_origin - m_horizontal / 2.0 - m_vertical / 2.0 - _focus_dist * m_w;
 		
 		m_lens_radius = _aperture / 2.0;
+		
+		m_time_begin = _time_begin;
+		m_time_end = _time_end;
 	}
 	
-	constexpr Rayd get_ray(double s, double t) const {
+	inline Rayd get_ray(double s, double t) const {
 		vec3 rd = m_lens_radius * vec3d::random_in_unit_disk();
 		vec3 offset = m_u * rd.x() + m_v * rd.y();
-		return Ray(m_origin + offset, m_low_lef_corner + s * m_horizontal + t * m_vertical - m_origin - offset);
+		return Ray(
+		m_origin + offset,
+		m_low_lef_corner + s * m_horizontal + t * m_vertical - m_origin - offset,
+		Random::range(m_time_begin, m_time_end));
 	}
 	
 private:
@@ -48,5 +55,7 @@ private:
 	vec3d m_low_lef_corner;
 	vec3d m_u, m_v, m_w;
 	double m_lens_radius;
+	double m_time_begin;
+	double m_time_end;
 	
 };
