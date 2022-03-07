38cd07553f4196bd79f7f718334316eb830d3fde
Slemmie <thorvejeneriksen@gmail.com>
2022-03-07 15:52:13 +0100

    add moving_sphere object

---
+++ v2.0/src/movable_sphere.h
@@ -0,0 +1,42 @@
+// movable sphere surface
+
+#pragma once
+
+#include "util/util.h"
+#include "ray.h"
+#include "hittable.h"
+
+// linearly movable
+class Moving_sphere : public hittable {
+	
+public:
+	
+	Moving_sphere() { }
+	
+	Moving_sphere(vec3d _center_begin, vec3d _center_end, double _time_begin, double _time_end,
+	double _radius, std::shared_ptr <Material> _material) :
+	m_center_begin(_center_begin),
+	m_center_end(_center_end),
+	m_time_begin(_time_begin),
+	m_time_end(_time_end),
+	m_radius(_radius),
+	m_material(_material)
+	{ }
+	
+	virtual bool hit(const Rayd& ray, double t_min, double t_max, Hit_record& hit_record) const override;
+	
+	constexpr const vec3d& center(double time) const {
+		return m_center_begin + ((time - m_time_begin) / (m_time_end - m_time_begin)) *
+		(m_center_end - m_center_begin);
+	}
+	
+	constexpr double radius() const { return m_radius; }
+	
+private:
+	
+	vec3d m_center_begin, m_center_end;
+	double m_time_begin, m_time_end;
+	double m_radius;
+	std::shared_ptr <Material> m_material;
+	
+};
