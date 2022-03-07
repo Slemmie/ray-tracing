3023ecfc53e9e09d5bdd99d551cb1157d73e3643
Slemmie <thorvejeneriksen@gmail.com>
2022-03-07 16:24:46 +0100

    add AABB prototypes

--- v2.0/src/AABB.h
+++ v2.0/src/AABB.h
@@ -8,10 +8,18 @@ class AABB {
 	
 public:
 	
+	constexpr AABB() { }
 	
+	constexpr AABB(const vec3d& _u, const vec3d& _v) :
+	m_minv(_u),
+	m_maxv(_v)
+	{ }
+	
+	bool hit(const Rayd& ray, double t_min, double t_max) const;
 	
 private:
 	
-	
+	vec3d m_minv;
+	vec3d m_maxv;
 	
 };
